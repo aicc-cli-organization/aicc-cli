d305c5a8565b3ebc7cfd7d8ea0356408ae22cb0f
zhoujie@yiwise.com <zhoujie@yiwise.com>
2019-12-17 19:38:34 +0800

    项目创建

---
+++ nuxt.config.js
@@ -0,0 +1,145 @@
+const pkg = require('./package')
+const nodeExternals = require('webpack-node-externals')
+const webpack = require('webpack')
+const path = require('path')
+
+function resolve (dir) {
+  return path.join(__dirname, '.', dir)
+}
+
+module.exports = {
+  mode: 'spa',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: ['element-ui/lib/theme-chalk/index.css'],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: ['@/plugins/element-ui'],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://github.com/nuxt-community/axios-module#usage
+    '@nuxtjs/axios'
+  ],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    // plugins: [
+    //   new webpack.DllReferencePlugin({
+    //     context: path.resolve('./crm/build'),
+    //     manifest: require('./crm/build/vendor-manifest.json')
+    //   })
+    // ],
+    extend(config, { isDev, isClient, isServer }) {
+      // Run ESLint on save
+      if (isDev && isClient) {
+        // config.module.rules.push({
+        //   enforce: 'pre',
+        //   test: /\.(js|vue)$/,
+        //   loader: 'eslint-loader',
+        //   exclude: /(node_modules)/
+        // })
+        // config.externals = [
+        //   nodeExternals({ whitelist: [
+        //     /^go.js/,
+        //     /^sip.js/,
+        //     /^jszip.js/,
+        //     /^FileSaver.js/,
+        //     /^element-ui/,
+        //     /^axios/,
+        //     /^echarts/,
+        //     /^vue-chat-scroll/,
+        //     /^js-cookie/,
+        //     /^vue/,
+        //     /^vue-i18n/,
+        //     /^vue-router/,
+        //     /^vuex/,
+        //     /^moment/,
+        //     /^sortablejs/,
+        //     /^qs/,
+        //     /^wavesurfer.js/,
+        //     /^vuedraggable/,
+        //     /^underscore/
+        //   ] })
+        // ]
+      }
+
+      config.resolve.alias['@'] = path.resolve('./crm/src')
+      config.resolve.alias['~'] = path.resolve('./crm/src')
+      config.resolve.alias['vue$'] = 'vue/dist/vue.esm.js'
+      config.resolve.alias['src'] = resolve('crm/src')
+      console.log(resolve('crm/src'))
+
+      if (isServer) {
+        // config.externals = [
+        //   nodeExternals({ whitelist: [
+        //     /^go.js/,
+        //     /^sip.js/,
+        //     /^jszip.js/,
+        //     /^FileSaver.js/,
+        //     /^element-ui/,
+        //     /^axios/,
+        //     /^echarts/,
+        //     /^vue-chat-scroll/,
+        //     /^js-cookie/,
+        //     /^vue/,
+        //     /^vue-i18n/,
+        //     /^vue-router/,
+        //     /^vuex/,
+        //     /^moment/,
+        //     /^sortablejs/,
+        //     /^qs/,
+        //     /^wavesurfer.js/,
+        //     /^vuedraggable/,
+        //     /^underscore/
+        //   ] })
+        // ]
+
+        // config.plugins.push(
+        //   new webpack.DllReferencePlugin({
+        //     context: path.resolve('./crm/build'),
+        //     manifest: require('./crm/build/vendor-manifest.json')
+        //   })
+        // )
+      }
+
+      
+      // console.log(config)
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="container">
+    <myCustomer />
+  </section>
+</template>
+
+<script>
+import Logo from '@@/components/Logo.vue'
+import myCustomer from '../crm/src/views/customer/PrivateSea/index.vue'
+import '../crm/src/main.js'
+import Vue from 'vue'
+console.log('pages index')
+
+export default {
+  components: {
+    Logo,
+    myCustomer
+  },
+  mounted() {
+    console.log('outter this')
+    console.log(this)
+    console.log(this.$store)
+    console.log(Vue)
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  /* display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center; */
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
