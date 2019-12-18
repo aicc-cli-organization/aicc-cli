66e46145175133962df4a890be4fd3290c100b09
zhoujie@yiwise.com <zhoujie@yiwise.com>
2019-12-18 10:18:04 +0800

    postcss包版本修改

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,13 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'stylesheet',
+        href: '//at.alicdn.com/t/font_747212_d4zc1jnlpi7.css'
+      }
+    ]
   },
   /*
   ** Customize the progress-bar color
