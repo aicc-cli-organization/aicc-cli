5a2611fcf52cccde8707043305e5507e4481f2f5
zhoujie@yiwise.com <zhoujie@yiwise.com>
2019-12-24 21:23:26 +0800

    删除subtree

--- aicc-components/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as BaseTopNav } from './BaseTopNav'
-export { default as MultiColorIcon } from './MultiColorIcon'
-export { default as Layout } from './Layout'
-export { default as WSPaginationTable } from './WSPaginationTable'
-export { default as YiwiseDialog } from './YiwiseDialog'
--- crm/src/api/phone.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function fetchPhoneLocation(params) {
-  return request({
-    url: '/apiEngine/phoneLocation/query',
-    method: 'get',
-    params
-  })
-}
