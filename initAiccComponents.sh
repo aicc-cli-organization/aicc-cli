97c898d7b6171d2f8ae9c9fad46aa296b9d3a080
zhoujie@yiwise.com <zhoujie@yiwise.com>
2019-12-30 09:50:08 +0800

    组件库地址修改

--- initAiccComponents.sh
+++ initAiccComponents.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git remote add aicc-components http://gitlab.yiwise.local/ai-call-platform/front-project/aicc-components.git
+git remote add aicc-components http://gitlab.yiwise.local/ai-contact-center/aicc-components.git
 git subtree add --prefix=aicc-components aicc-components develop --squash
